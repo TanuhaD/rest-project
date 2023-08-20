50fb1a4299dbb11a18c1f049ff4257e0bee2743b
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-08-21 01:16:54 +0300

    Home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Hero from "../../components/Hero/Hero";
+import SpecialOffers from "../../components/SpecialOffers/SpecialOffers";
+import WhyChooseUs from "../../components/WhyChooseUs/WhyChooseUs";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <WhyChooseUs />
+      <SpecialOffers />
+    </>
+  );
+};
+
+export default Home;
