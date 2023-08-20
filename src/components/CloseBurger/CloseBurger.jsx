59f8f1a8f85e021eb463a3a86e655887d4df6b67
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-08-21 01:09:25 +0300

    CloseBurger.jsx

---
+++ src/components/CloseBurger/CloseBurger.jsx
@@ -0,0 +1,12 @@
+const CloseBurger = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={26}
+    height={26}
+    fill="none"
+    {...props}
+  >
+    <path stroke="#333" strokeWidth={2} d="M1 25 25 1m0 24L1 1" />
+  </svg>
+);
+export default CloseBurger;
